a7e7810824dc1dd88dd67e2c32d60f1647b88e36
naomi-ho <151114635+naomi-ho@users.noreply.github.com>
2024-10-17 13:19:25 -0400

    Add class names

--- src/modules/home.js
+++ src/modules/home.js
@@ -1,6 +1,7 @@
 export function addHomeElement() {
   // create new div
   const homeDiv = document.createElement('div');
+  homeDiv.className = 'home-details';
 
   // restaurant name
   const restaurantName = document.createElement('h1');
@@ -11,10 +12,12 @@ export function addHomeElement() {
   const tagLine = document.createElement('p');
   tagLine.textContent =
     'Just another matcha cafe incorporating ceremonial-grade matcha powder into tasty items.';
+  tagLine.className = 'tag-line';
 
   // address
   const address = document.createElement('p');
   address.textContent = '420 Green St, New York, NY 10008';
+  address.className = 'address';
 
   // add elements to new div
   homeDiv.appendChild(restaurantName);
